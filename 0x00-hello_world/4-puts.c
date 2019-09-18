6e979dc47ebe4765a006ab4c39b4386b03a0bea1
Ben Keener <keener4christ@gmail.com>
2019-09-18 18:14:03 +0000

    Completed task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Prints a predefined string
+ *
+ * Return: 0 for success
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
