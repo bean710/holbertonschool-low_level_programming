dd9f80a7390631a0c890973aac420dfffcc778e5
Ben Keener <keener4christ@gmail.com>
2019-09-19 16:48:08 +0000

    Added program that prints random number, its last digit, and whether it passes some checks

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Outputs a random number, its last digit, and whether the last digit
+ *	is less than 6 (but not 0), is greater than 5, and/or is 0
+ *
+ * Return: 0 (Always success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	int last = n % 10;
+
+	printf("The last digit of %d is %d", n, last);
+	if (last > 5)
+	{
+		printf(" and is greater than 5");
+	}
+	if (last == 0)
+	{
+		printf(" and is 0");
+	}
+	if ((last < 6) && (last != 0))
+	{
+		printf(" and is less than 6 and not 0");
+	}
+	printf("\n");
+
+	return (0);
+}
