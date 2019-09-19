66b7b55e733753015f59ad444f9d0b6d278e31fe
Ben Keener <keener4christ@gmail.com>
2019-09-19 18:20:04 +0000

    Made betty-compliant

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 
-int main (void)
+/**
+ * main - Prints lowercase alphabet
+ *
+ * Return: 0 Always success
+ */
+int main(void)
 {
 	char c = 'a';
 
 	while (c <= 'z')
 		putchar(c++);
-	
+
 	putchar('\n');
+
+	return (0);
 }
