dceeb2dbc561c54bf939d014e19e66db9faa7ab1
Ben Keener <keener4christ@gmail.com>
2019-09-19 18:23:30 +0000

    Program outputs lowercase then uppercase alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Prints lowercase alphabet and then uppercase alphabet
+ *
+ * Return: 0 Always success
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+		putchar(c++);
+	
+	c = 'A';
+	while (c <= 'Z')
+		putchar(c++);
+
+	putchar('\n');
+
+	return (0);
+}
