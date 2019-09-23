f212b7a5fc6c5daa4744f8a702202dcbdd1b3df2
Ben Keener <keener4christ@gmail.com>
2019-09-23 18:34:01 +0000

    Function checks if number is positive, negative or zero

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * print_sign - prints +, 0, or - based on positive or negative
+ * @n: number o=to check
+ *
+ * Return: 1 for positive, 0 for zero, -1 for negative
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
