940dfbe9d39a9cfde361a3a2eeaddee6e6434bbb
Ben Keener <keener4christ@gmail.com>
2019-09-23 18:44:57 +0000

    Function returns absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * _abs - Calculates the absolute value
+ * @n: number to be calculated
+ *
+ * Return: Absolute value of n
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
