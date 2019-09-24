1c521c36b53ef5b071d2aee18a111e2dfe38841c
Ben Keener <keener4christ@gmail.com>
2019-09-24 18:13:32 +0000

    Completed task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * add - adds two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: the sum of the two numbers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
