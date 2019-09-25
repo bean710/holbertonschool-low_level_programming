76f314d0958af9ab2ad1ff95c82e5355b5c8e8d7
Ben Keener <keener4christ@gmail.com>
2019-09-25 01:17:38 +0000

    Added documentation

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 
-
+/**
+ * main - Prints the sum of all multiples of 3 or 5 under 1024
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
 	int i, s;
 
-	s = 0; 
+	s = 0;
 	for (i = 0; i < 1024; i += 3)
 		s += i;
 
