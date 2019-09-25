fe1b25d29d6d2771b578e81b1783da7a43462ae3
Ben Keener <keener4christ@gmail.com>
2019-09-25 01:12:00 +0000

    Completed task 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+	int i, s;
+
+	s = 0; 
+	for (i = 0; i < 1024; i += 3)
+		s += i;
+
+	for (i = 0; i < 1024; i += 5)
+	{
+		if (i % 3 == 0)
+			s += i;
+	}
+
+	printf("%d\n", s);
+
+	return (0);
+}
