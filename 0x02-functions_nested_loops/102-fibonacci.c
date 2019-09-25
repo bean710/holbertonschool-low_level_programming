3c55faa68953cac7f21ca90d15f7cc2fe9f16851
Ben Keener <keener4christ@gmail.com>
2019-09-25 01:03:14 +0000

    Completed task 12

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - prints first 50 fibonacci numbers
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	unsigned long a, b, c;
+	int i;
+
+	a = 1;
+	b = 2;
+
+	printf("1, 2");
+	for (i = 0; i < 48; i++)
+	{
+		c = a + b;
+		printf(", %lu", c);
+		a = b;
+		b = c;
+	}
+	printf("\n");
+
+	return (0);
+}
