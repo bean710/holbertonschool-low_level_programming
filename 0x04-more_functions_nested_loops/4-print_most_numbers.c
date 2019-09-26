069b69e62bdf11bebbdb74dddd8b7460572e780f
Ben Keener <keener4christ@gmail.com>
2019-09-26 16:26:57 +0000

    Updated documentation

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,14 +1,17 @@
 #include "holberton.h"
 
 /**
- * print_numbers - Prints digits 0 through 9
+ * print_numbers - Prints digits 0 through 9, excluding 2 and 4
  */
 void print_numbers(void)
 {
 	char c;
 
 	for (c = '0'; c <= '9'; c++)
-		_putchar(c);
+	{
+		if (c != '2' && c != '4')
+			_putchar(c);
+	}
 
 	_putchar('\n');
 }
