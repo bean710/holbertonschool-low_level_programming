7835a8686fb17b590ec23b53dadc24e06efb0d9c
Ben Keener <keener4christ@gmail.com>
2019-09-26 17:36:56 +0000

    Completed task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * print_triangle - Prints a triangle with base and leg length `size`
+ * @size: size of the triangle
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+		_putchar('\n');
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			if (j < size - i - 1)
+				_putchar(' ');
+			else
+				_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
