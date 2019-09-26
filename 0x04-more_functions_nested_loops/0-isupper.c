06e54506344dfdef51c1f60a643db66d9490de1f
Ben Keener <keener4christ@gmail.com>
2019-09-26 16:08:30 +0000

    Completed task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * _isupper - Checks if character is an uppercase letter
+ * @c: Character to check 
+ *
+ * Return: 1 if uppercase, 0 otherwise
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+
+	return (0);
+}
