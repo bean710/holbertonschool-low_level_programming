355657b80078ba6ce6c04bc99e601fad29dde846
Ben Keener <keener4christ@gmail.com>
2019-09-26 16:37:53 +0000

    Completed task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * print_line - Prints line of length n
+ * @n: Length of line
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		_putchar('_');
+
+	_putchar('\n');
+}
