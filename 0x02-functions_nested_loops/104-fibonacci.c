51e37218ea737ef4087c20176737669f025d9e96
Ben Keener <keener4christ@gmail.com>
2019-09-26 22:03:56 +0000

    Added constant max in response to #9

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,6 +7,7 @@
  */
 int main(void)
 {
+	const unsigned long int max;
 	unsigned long int a, ao;
 	unsigned long int b, bo;
 	unsigned long int c, co;
@@ -19,15 +20,17 @@ int main(void)
 	c = 0;
 	co = 0;
 
+	max = 1000000000000000000;
+
 	printf("1, 2");
 	for (i = 0; i < 96; i++)
 	{
 		c = a + b;
 		co = ao + bo;
-		if (c > 999999999999999999)
+		if (c > max - 1)
 		{
 			co += 1;
-			c %= 1000000000000000000;
+			c %= max;
 		}
 		a = b;
 		ao = bo;
