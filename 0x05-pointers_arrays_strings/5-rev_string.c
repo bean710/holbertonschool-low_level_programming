c432f15baf93dade40989980711d187e742e9ad8
Ben Keener <keener4christ@gmail.com>
2019-10-01 16:55:57 +0000

    Completed task 6

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * rev_string - Prints a string backwards
+ * @s: pointer to the first character in the string
+ */
+void rev_string(char *s)
+{
+	int len, i;
+	char *str;
+
+	for (len = 0; *s; s++)
+		len++;
+
+	str = malloc(sizeof(char) * len);
+	s--;
+
+	for (i = 0; i < len; i++)
+		str[i] = *(s--);
+	s++;
+
+	str[i] = '\0';
+
+	printf("%s\n", str);
+
+
+	for (i = 0; str[i]; i++)
+		s[i] = str[i];
+
+	s[i] = '\0';
+}
