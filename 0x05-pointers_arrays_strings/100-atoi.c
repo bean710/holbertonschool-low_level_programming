a4dce34166bbd8c7acf170088c1e58994c87c109
Ben Keener <keener4christ@gmail.com>
2019-10-01 20:28:47 +0000

    completed task 10

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+
+/**
+ * _atoi - Converts a string to an integer
+ * @s: pointer to the first character of the string
+ *
+ * Return: Value of integer in string
+ */
+int _atoi(char *s)
+{
+	int neg, num;
+
+	num = 0;
+
+	for (; *s; s++)
+	{
+		if (*s >= '0' && *s <= '9')
+		{
+			num *= 10;
+			num += *s - '0';
+		}
+		else if (num > 0)
+		{
+			break;
+		}
+		else if (*s == '-')
+		{
+			neg--;
+		}
+		else if (*s == '+')
+		{
+			neg++;
+		}
+	}
+
+	return ((neg < 0) ? -num : num);
+}
