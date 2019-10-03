5076878939c2c740c1c930c84bc3063e43486474
Ben Keener <keener4christ@gmail.com>
2019-10-03 18:15:31 +0000

    Completed task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * cap_string - Capitalizes the first letter of each word of a string
+ * @a: pointer to string
+ *
+ * Return: pointer to beginning of string
+ */
+char *cap_string(char *a)
+{
+	char *seps = " \t\n,;.!?\"(){}";
+	char *s = seps;
+	char *p = a;
+	char *tmp;
+	int diff = ('a' - 'A');
+
+	for (; *p; p++)
+	{
+		for (s = seps; *s; s++)
+		{
+			if (*p == *s)
+			{
+				tmp = p + 1;
+				if (*tmp >= 'a' && *tmp <= 'z')
+					*tmp -= diff;
+			}
+		}
+	}
+
+	return (a);
+}
