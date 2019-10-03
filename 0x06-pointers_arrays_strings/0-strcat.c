089bb3f4b83be9c26ec80a9f4c59618b407e0ffd
Ben Keener <keener4christ@gmail.com>
2019-10-03 16:33:12 +0000

    Completed task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * _strcat - Concatonates two strings
+ * @dest: Pointer to both the first sting and the destination for the
+ *	  Concatonated string
+ * @src: Pointer to the second string
+ *
+ * Return: Pointer to the resulting string
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *p = dest;
+
+	for (; *p; p++)
+		;
+
+	for (; *src; src++, p++)
+		*p = *src;
+
+	return (dest);
+}
