9a22e7c309f8354616b5adc6028901a1035938a3
Ben Keener <keener4christ@gmail.com>
2019-10-07 17:36:12 +0000

    Completed task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stddef.h>
+
+/**
+ * _strstr - Finds first instance of `needle` in `haystack`
+ * @haystack: the string to be searched in
+ * @needle: the substring to search for
+ *
+ * Return: pointer to first character of substring within string, or NULL if
+ *		   not found
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+
+	for (; *haystack; haystack++)
+	{
+		for (i = 0; needle[i]; i++)
+		{
+			if (needle[i] != haystack[i])
+				break;
+		}
+
+		if (needle[i] == '\0')
+			return (haystack);
+	}
+
+	return (NULL);
+}
