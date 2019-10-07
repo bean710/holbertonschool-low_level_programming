1c2d432cc79c0e22d0f06d0649e85afa609c6eaf
Ben Keener <keener4christ@gmail.com>
2019-10-07 17:08:48 +0000

    Completed task 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: pointer to beginning of string
+ * @accept: list of acceptable characters for the prefix
+ *
+ * Return: Length of prefix
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int count;
+	unsigned int i, j;
+
+	for (i = 0; *s; s++, i++)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (*s == accept[j])
+			{
+				count++;
+				break;
+			}
+		}
+
+		if (count <= i)
+			break;
+	}
+
+	return (count);
+}
