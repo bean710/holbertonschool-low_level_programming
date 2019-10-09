27a339078c2fe91aefb3bb86898a7589d68924f3
Ben Keener <keener4christ@gmail.com>
2019-10-09 17:00:59 +0000

    Completed task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * _strlen_recursion - gets the length of a string using recursion
+ * @s: pointer to character in string
+ *
+ * Return: length of string
+ */
+int _strlen_recursion(char *s)
+{
+	if (!*s)
+		return 0;
+
+	return (_strlen_recursion(s + 1) + 1);
+}
