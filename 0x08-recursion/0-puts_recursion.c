7161a52858abf8c24f85efc457aec5ac35932377
Ben Keener <keener4christ@gmail.com>
2019-10-09 16:43:30 +0000

    Added docs

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,5 +1,9 @@
 #include "holberton.h"
 
+/**
+ * _puts_recursion - uses _putchar recursively to print a string
+ * @s: pointer to first char in string to be printed
+ */
 void _puts_recursion(char *s)
 {
 	if (*s)
