72c26bfa6dd05631595298388e43e0af5f74c2d6
Ben Keener <keener4christ@gmail.com>
2019-10-12 02:46:19 +0000

    Completed task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#define UNUSED(x) (void)(x)
+
+/**
+ * main - Prints the number of arguments, not including command name
+ * @argc: number of arguments including command name
+ * @argv: Pointer to array of pointers to arguments as strings including
+ *			command name
+ *
+ * Return: 0 for success, 1 for error
+ */
+int main(int argc, char **argv)
+{
+	UNUSED(argv);
+	printf("%d\n", argc - 1);
+}
