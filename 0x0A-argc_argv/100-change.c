3de403934b3f694d11c910640ce0dfaf2f906cec
Ben Keener <keener4christ@gmail.com>
2019-10-13 05:44:27 +0000

    Completed task 5

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+
+/**
+ * _atoi - Converts a string to an integer
+ * @s: pointer to the first character of the string
+ *
+ * Return: Value of integer in string
+ */
+int _atoi(char *s)
+{
+	unsigned int num;
+	int neg;
+
+	neg = 1;
+	num = 0;
+
+	for (; *s; s++)
+	{
+		if (*s >= '0' && *s <= '9')
+		{
+			num *= 10;
+			num += *s - '0';
+		}
+		else if (num > 0)
+		{
+			break;
+		}
+		else if (*s == '-')
+		{
+			neg = -neg;
+		}
+	}
+
+	return (num * neg);
+}
+
+int main(int argc, char **argv)
+{
+	int input, coinnum = 0;
+	int coins[5] = {25, 10, 5, 2, 1};
+	int i;
+
+	if (argc != 2)
+	{
+		puts("Error");
+		return (1);
+	}
+
+	input = _atoi(argv[1]);
+
+	if (input < 0)
+	{
+		puts("Error");
+		return (1);
+	}
+
+	for (i = 0; i < 5; i++)
+	{
+		while (input - coins[i] >= 0)
+		{
+			coinnum++;
+			input -= coins[i];
+		}
+	}
+
+	printf("%d\n", coinnum);
+
+	return (1);
+}
