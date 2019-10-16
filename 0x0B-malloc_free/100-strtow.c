22d9b23507367fd63f111bdfd566d9226b5c0420
Ben Keener <keener4christ@gmail.com>
2019-10-16 23:44:52 +0000

    completed task 6

---
+++ 0x0B-malloc_free/100-strtow.c
@@ -0,0 +1,91 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * freememc - frees the memory of an array of pointers pointing to arrays
+ * @d: pointer to first pointer in array
+ */
+void freememc(char **d)
+{
+	int i;
+
+	for (i = 0; d[i] != NULL; i++)
+	{
+		free(d[i]);
+	}
+	free(d);
+}
+
+/**
+ * get_wordcount - gets the number of words in a string with space separators
+ * @c: pointer to the first character in the string
+ *
+ * Return: Number of words in the string
+ */
+int get_wordcount(char *c)
+{
+	int i, count = 0;
+
+	for (i = 0; c[i]; i++)
+	{
+		if (c[i] != ' ')
+		{
+			count++;
+			for (; c[i] && c[i] != ' '; i++)
+				;
+		}
+	}
+
+	return (count);
+}
+
+/**
+ * strtow - converts a sting to an array of the words in the string
+ * @str: Pointer to the first character in the original string
+ *
+ * Return: A pointer to the first pointer in an array of pointers which each
+ *			point to the first character in the string of a word
+ */
+char **strtow(char *str)
+{
+	char **dest;
+	int wordcount, onword = 0, wordsize;
+	int i, j, w;
+
+	if (str == NULL)
+		return (NULL);
+	if (str[0] == '\0')
+		return (NULL);
+
+	wordcount = get_wordcount(str);
+
+	dest = malloc(sizeof(char *) * (wordcount + 1));
+	if (dest == NULL)
+		return (NULL);
+
+	for (i = 0; str[i]; i++)
+	{
+		if (str[i] == ' ')
+			continue;
+
+		for (j = i; str[j] && str[j] != ' '; j++)
+			;
+
+		wordsize = j - i;
+		dest[onword] = malloc(sizeof(char) * (wordsize + 1));
+		if (dest[onword] == NULL)
+		{
+			freememc(dest);
+			return (NULL);
+		}
+
+		for (w = 0; str[i] && str[i] != ' '; i++, w++)
+			dest[onword][w] = str[i];
+		dest[onword][w] = '\0';
+
+		onword++;
+	}
+	dest[onword] = NULL;
+
+	return (dest);
+}
--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -9,5 +9,8 @@ int **alloc_grid(int width, int height);
 void freemem(int **);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
+char **strtow(char *str);
+int get_wordcount(char *str);
+void freememc(char **d);
 
 #endif
