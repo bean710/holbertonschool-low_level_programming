510929923b04edf65e90fcf8d7b225f20e60319d
Ben Keener <keener4christ@gmail.com>
2019-10-17 23:48:32 +0000

    If new_size is null, ptr is freed

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -21,6 +21,12 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size == old_size)
 		return (ptr);
 
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
 	new = malloc(new_size);
 
 	if (new == NULL)
