404389180ceb0f78a6aa762e9399e1468181b81c
Ben Keener <keener4christ@gmail.com>
2019-10-17 19:51:21 +0000

    Changed memory allocation to use minimu required

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -6,13 +6,18 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *cat;
 	unsigned int i, count = 0;
+	unsigned int len;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-	cat = malloc(sizeof(char) * (_strlen(s1) + _strlen(s2) + 1));
+	len = _strlen(s2);
+	if (_strlen(s2) > n)
+		len = n;
+
+	cat = malloc(sizeof(char) * (_strlen(s1) + len + 1));
 
 	if (cat == NULL)
 		return (NULL);
