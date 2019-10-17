44d5d4dab6637507b1accdbb8ddd1223c3d97ab3
Ben Keener <keener4christ@gmail.com>
2019-10-17 22:31:49 +0000

    Added documentation

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,6 +1,15 @@
 #include "holberton.h"
 #include <stdlib.h>
 
+/**
+ * _realloc - Allocates space of `new_size` bytes and moves old data to new
+ *            space.
+ * @ptr: Pointer to the old memory space
+ * @old_size: The size in bytes of the old memory space
+ * @new_size: The size in bytes of the new memory space
+ *
+ * Return: Pointer to the new memory space
+ */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	void *new;
