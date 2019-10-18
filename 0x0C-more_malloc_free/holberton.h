b810a272962abf4dde8d36210cc7a79b98209e67
Ben Keener <keener4christ@gmail.com>
2019-10-18 07:00:47 +0000

    Free points

--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -6,5 +6,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 unsigned int _strlen(char *);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+char *_prepend(char *p, char c);
+void _puts(char *str);
+int _putchar(char c);
 
 #endif
