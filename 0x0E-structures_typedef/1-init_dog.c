094c81f56307ba9b23bcd348a61a55f5fca277be
Ben Keener <keener4christ@gmail.com>
2019-10-21 21:29:36 +0000

    Added case for  being null

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,4 @@
 #include "dog.h"
-#include "holberton.h"
 
 /**
  * init_dog - Initializes a dog struct
@@ -10,6 +9,9 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+		return;
+
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
