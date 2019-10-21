f96587cf5eb001acd431f1248f06767552de4f37
Ben Keener <keener4christ@gmail.com>
2019-10-21 23:44:58 +0000

    Added documentation

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,14 @@
 #include "dog.h"
 #include <stdlib.h>
 
+/**
+ * dup_string - Duplicates a string passed into a newly allocated space in
+ * memory
+ * @s: Pointer to the first character in the source string
+ *
+ * Return: Pointer to the first character in the newly allocated and copied
+ * string
+ */
 char *dup_string(char *s)
 {
 	int len, i;
@@ -17,9 +25,19 @@ char *dup_string(char *s)
 		ret[i] = s[i];
 	ret[i] = '\0';
 
-	return(ret);
+	return (ret);
 }
 
+/**
+ * new_dog - Makes a new dog with the given parameters
+ * @name: Pointer to the first character of the string of the name of the dog
+ * to be created
+ * @age: Age of the dog to be created as a float
+ * @owner: Pointer to the first character of the string of the owner of the dog
+ * to be created
+ *
+ * Return: Pointer to the new `dog_t` with all of the given attributes
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	char *tname, *towner;
@@ -29,7 +47,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (ret == NULL)
 		return (NULL);
 	ret->age = age;
-	
+
 	tname = dup_string(name);
 	if (tname == NULL)
 		return (NULL);
