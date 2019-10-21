f50a2290f27e6afd8b71e719922240f2bfc3ccaf
Ben Keener <keener4christ@gmail.com>
2019-10-21 21:08:02 +0000

    Added documentation

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef _DOG_
 #define _DOG_
 
+/**
+ * struct dog - Dog with a few attributes
+ * @name: The name of the dog as a pointer to the first character of a string
+ * @age: The age of the dog aas a float
+ * @owner: The name of the owner of the dog as a pointer to the first character
+ * of a string
+ */
 struct dog
 {
 	char *name;
