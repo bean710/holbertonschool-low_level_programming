6d673e6f89a1f6ef569f14eae7b9cf8d197f375b
Ben Keener <keener4christ@gmail.com>
2019-10-23 14:49:47 +0000

    Completed task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,16 @@
+#include <stddef.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - Runs a given function on each item in an array
+ * @array: Pointer to the first integer in an array of integers
+ * @size: Size of `array`
+ * @action: Pointer to the function to be called on each item in the array
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
