ccca3e69eb260ad4dddf544aaf6767582f6010b6
Ben Keener <keener4christ@gmail.com>
2019-10-23 20:08:46 +0000

    Updated header

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,8 @@
 #ifndef _HOLBERTON_
 #define _HOLBERTON_
 
+#include <stddef.h>
+
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
