c8e6c640fc9417c05a240ce7a974842346eb6854
Ben Keener <keener4christ@gmail.com>
2019-10-23 14:34:22 +0000

    Completed task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - Prints a string using the provided function
+ * @name: Pointer to the first character in the string
+ * @f: Pointer to the function to be used to print
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
