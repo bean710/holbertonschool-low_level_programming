d4148e642eb50fb21a3ad988eb584a2cd5affffa
Ben Keener <keener4christ@gmail.com>
2019-10-25 17:13:25 +0000

    Added documentation

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -2,6 +2,13 @@
 #include <stdio.h>
 #include "variadic_functions.h"
 
+/**
+ * print_strings - Prints `n` number of given strings, separated by the given
+ * separator
+ * @separator: Pointer to the first character of the string to be used as the
+ * separator
+ * @n: Number of strings of the strings given to be printed
+ */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
