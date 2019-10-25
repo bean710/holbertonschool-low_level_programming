7ca0b2416511157dac6405f287a08502a0528741
Ben Keener <keener4christ@gmail.com>
2019-10-25 18:10:35 +0000

    Added documentation

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,11 +2,22 @@
 #include <stdio.h>
 #include "variadic_functions.h"
 
+/**
+ * is_match - Checks if a character matches the set
+ * @c: The character to check
+ *
+ * Return: 0 if no match, nonzero if match
+ */
 int is_match(char c)
 {
 	return (c == 'c' || c == 'i' || c == 'f' || c == 's');
 }
 
+/**
+ * print_all - Prints a list of items of several types
+ * @format: A pointer to the first character in the string that gives the
+ * type and position of each argument
+ */
 void print_all(const char * const format, ...)
 {
 	unsigned int i = 0;
@@ -20,7 +31,7 @@ void print_all(const char * const format, ...)
 	while (format[i])
 	{
 		if (i != 0 && is_match(format[i]))
-			printf (", ");
+			printf(", ");
 
 		switch (format[i])
 		{
