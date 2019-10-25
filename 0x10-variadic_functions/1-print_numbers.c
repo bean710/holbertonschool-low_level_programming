d0b8e7e846d6c13fd9f150670c62af4f8de6b21e
Ben Keener <keener4christ@gmail.com>
2019-10-25 17:01:56 +0000

    Added documentation

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -2,6 +2,12 @@
 #include <stdio.h>
 #include "variadic_functions.h"
 
+/**
+ * print_numbers - Prints `n` numbers given, separated by a given separator
+ * @separator: Pointer to the first character of the string to separate each
+ * integer
+ * @n: Number of integers of the given integers to be printed
+ */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list valist;
