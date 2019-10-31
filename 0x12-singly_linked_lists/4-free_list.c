15163a7f0e985c0fa69a5a054ef880b51381419a
Ben Keener <keener4christ@gmail.com>
2019-10-31 22:41:47 +0000

    Added documentation

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,6 +1,10 @@
 #include "lists.h"
 #include <stddef.h>
 
+/**
+ * free_list - Frees a linked list of list_t's
+ * @head: Pointer to the first node
+ */
 void free_list(list_t *head)
 {
 	list_t *tmp;
