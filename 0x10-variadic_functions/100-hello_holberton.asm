9d5f08d577defc1d9761efde98e1110bc0e19377
Ben Keener <keener4christ@gmail.com>
2019-11-01 00:51:51 +0000

    Completed task 4

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,16 +1,17 @@
 section .data
-    msg db "Hello world!",10      ; 10 is the ASCII code for a new line (LF)
+	msg: db "Hello, Holberton", 10
 
 section .text
-    global _start
 
-_start:
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, msg
-    mov rdx, 13
-    syscall
-    
-    mov rax, 60
-    mov rdi, 0
-    syscall
+global main
+main:
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, msg
+	mov rdx, 17
+	syscall
+
+	mov rax, 60
+	mov rdi, 0
+	syscall
+
