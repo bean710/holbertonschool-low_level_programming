0f64993533664e27312564059a0cfb19aa850e7c
Ben Keener <keener4christ@gmail.com>
2019-11-04 23:55:23 +0000

    Completed task 13

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * free_listint_safe - Frees a linked list of listint_t's
+ * @head: Pointer to a pointer pointing to the first node in the list
+ *
+ * Return: The size of the free'd list
+ */
+size_t free_listint_safe(listint_t **head)
+{
+	listint_t *next;
+	size_t len;
+
+	if (!head || !(*head))
+		return (0);
+
+	for (len = 0; *head; *head = next, len++)
+	{
+		next = (*head)->next;
+		if ((void *)next > (void *)*head)
+		{
+			free(*head);
+			*head = NULL;
+			return (len + 1);
+		}
+
+		free(*head);
+	}
+
+	return (len);
+}
