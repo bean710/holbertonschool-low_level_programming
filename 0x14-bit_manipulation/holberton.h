c061bd5afdc0eea18303a5c35b5d7c503bc9af4b
Ben Keener <keener4christ@gmail.com>
2019-11-07 22:38:56 +0000

    Completed task 3

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -2,11 +2,13 @@
 #define _BIT_MANIPULATION_
 
 #include <limits.h>
+#include <stddef.h>
 
 int _putchar(char);
 
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
 
 #endif
