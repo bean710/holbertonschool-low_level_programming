9fd436da09a081f7312f70d55e1c9a82c35121d5
Ben Keener <keener4christ@gmail.com>
2019-11-12 21:17:36 +0000

    Checked for successful read

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -22,6 +22,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	ret = read(desc, buffer, letters);
+	if (ret < 0)
+		return (0);
 
 	status = write(1, buffer, ret);
 	if (status < 0)
