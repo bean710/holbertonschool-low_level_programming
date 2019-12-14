9a9318f6accea30a8bc775741ad929a0e2cc3155
Ben Keener <keener4christ@gmail.com>
2019-12-14 22:36:28 +0000

    Added docs

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,5 +1,13 @@
 #include "lists.h"
 
+/**
+ * get_dnodeint_at_index - Gets a node at a given index
+ * @head: Address of the first node
+ * @index: Index of the desired node
+ *
+ * Return: Address of the node at the given index, or NULL if it does not
+ * exist
+ */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int on_index;
