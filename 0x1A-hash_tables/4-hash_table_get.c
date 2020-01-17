e00a339e8b1255db96db2f7ef3a339743ca14efe
Ben Keener <keener4christ@gmail.com>
2020-01-17 21:30:09 +0000

    Added function to get value from hash table

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,24 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * hash_table_get - Gets a value given a key from a hash table
+ * @ht: Pointer to the hash table
+ * @key: Pointer to the string key
+ *
+ * Return: The value found for the key, or NULL if no value
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index = key_index((const unsigned char *)key, ht->size);
+	hash_node_t *tmp;
+
+	for (tmp = ht->array[index]; tmp; tmp = tmp->next)
+	{
+		if (strcmp(tmp->key, key) == 0)
+			return (tmp->value);
+	}
+
+	return (NULL);
+}
