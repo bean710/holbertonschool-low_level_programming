41b36c5709a00fb75044d15d65bef0d55cd4b95e
Ben Keener <keener4christ@gmail.com>
2020-04-14 16:46:15 +0000

    Completed task 0

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,23 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - Search through an array linearly
+ * @array: Pointer to the first item in the array
+ * @size: The size of the array
+ * @value: The value to look for in the array
+ *
+ * Return: The index of the value if found, -1 otherwise
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%u] = [%d]\n", (unsigned int)i, array[i]);
+		if (array[i] == value)
+			return ((int)i);
+	}
+
+	return (-1);
+}
