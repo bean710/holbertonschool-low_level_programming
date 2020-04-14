7b90cbbd210d84d7b5ae85cf9f76df0764fa5cca
Ben Keener <keener4christ@gmail.com>
2020-04-14 20:18:48 +0000

    Completed task 11

---
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -0,0 +1,48 @@
+#include "search_algos.h"
+
+/**
+ * print_arr - Prints an array from `begin` to `end`
+ * @arr: Pointer to the beginning of the array
+ * @begin: Index to start printing at
+ * @end: Index to stop printing at
+ */
+void print_arr(int *arr, int begin, int end)
+{
+	if (begin <= end)
+		printf("Searching in array: ");
+	for (; begin <= end; begin++)
+	{
+		if (begin == end)
+			printf("%d\n", arr[begin]);
+		else
+			printf("%d, ", arr[begin]);
+	}
+}
+
+int advanced_binary(int *array, size_t size, int value)
+{
+	int mid, ret;
+
+	if (!array)
+		return (-1);
+
+	print_arr(array, 0, size - 1);
+
+	if (size == 1 && array[0] != value)
+		return (-1);
+	
+	mid = (size - 1) / 2;
+	if (array[mid] < value)
+	{
+		ret = advanced_binary(array + mid + 1, size / 2, value);
+		return (ret == -1 ? -1 : ret + mid + 1);
+	}
+	else if (array[mid] > value)
+		return (advanced_binary(array, size / 2 - 1, value));
+	else if (mid != 0)
+		return (advanced_binary(array, size / 2, value));
+	else if (array[mid] == value)
+		return (mid);
+
+	return (-1);
+}
