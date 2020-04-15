e8bf6f26600400c3e4cbe17796a2a56fe43430f3
Ben Keener <keener4christ@gmail.com>
2020-04-15 20:35:21 +0000

    Added out of range case

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -18,11 +18,16 @@ int interpolation_search(int *array, size_t size, int value)
 	if (!array)
 		return (-1);
 
-	while ((array[hi] != array[lo]) && (value >= array[lo]) &&
-			(value <= array[hi]))
+	while (array[hi] != array[lo] && mid >= 0)
 	{
 		mid = lo + (((double)(hi - lo) / (array[hi] - array[lo])) *
 				(value - array[lo]));
+
+		if (mid >= (int)size)
+		{
+			printf("Value checked array[%d] is out of range\n", mid);
+			return (-1);
+		}
 
 		printf("Value checked array[%d] = [%d]\n", mid, array[mid]);
 
